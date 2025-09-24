1b4338d37af88b5c4215b2053e016d7ed3f169d7
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2025-09-24 20:04:34 -0300

    Update Strada.js

--- class/Strada.js
+++ class/Strada.js
@@ -1,15 +1,16 @@
-const carros = require('./Carros');
+const Carros = require('./Carros')
 
-class Strada extends carros {
-    constructor(descricao){
-    super(descricao)
+class Strada extends Carros {
+    constructor() {
+        super(
+            "Strada ","A Fiat Strada é uma picape compacta, líder de vendas no Brasil há mais de duas décadas, reconhecida pela robustez e versatilidade." 
+        );
     }
 
-    acessoPainel(){
+    acessoPainel() {
         return `
-            A Fiat Strada é uma picape compacta, líder de vendas no Brasil há mais de duas décadas, reconhecida pela robustez e versatilidade. 
-            Sua capacidade de se adaptar tanto para o trabalho pesado quanto para o uso diário, com opções de cabine simples
-            e cabine dupla, a torna uma escolha popular entre diferentes perfis de consumidores, com o motor 1.3 e 1.0 Turbo  versões Endurance, Freedom, Volcano 👍`
+${this.nome}: ${this.descricao}
+Sua capacidade de se adaptar tanto para o trabalho pesado quanto para o uso diário, com opções de cabine simples e cabine dupla, a torna uma escolha popular entre diferentes perfis de consumidores, com motor 1.3 e 1.0 Turbo, versões Endurance, Freedom e Volcano. 👍`
     }
 }
 
