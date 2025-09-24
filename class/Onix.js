e43662b3b011a13e5b91e2ccf88a56ee8fb145a9
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2025-09-24 20:02:44 -0300

    Update Onix.js

--- class/Onix.js
+++ class/Onix.js
@@ -1,15 +1,17 @@
-const carros = require('./Carros');
+const Carros = require('./Carros')
 
-class Onix extends carros {
-    constructor(descricao){
-    super(descricao)
+class Onix extends Carros {
+    constructor() {
+        super(
+            "Onix ", "O Chevrolet Onix é um dos carros mais populares do Brasil, conhecido por seu bom custo-benefício, design moderno e tecnologia embarcada." 
+        )
     }
     
-    acessoPainel(){
+    acessoPainel() {
         return `
-            O Chevrolet Onix é um dos carros mais populares do Brasil, conhecido por seu bom custo-benefício, design moderno e tecnologia embarcada.
-            Ele está disponível nas versões hatch e sedã, com  versões (LT, LTZ TURBO, RS Turbo, Premier ) opções de motores 1.0 aspirado ou turbo, e diferentes pacotes de equipamentos. 👍`
+${this.nome}: ${this.descricao}
+Ele está disponível nas versões hatch e sedã (LT, LTZ TURBO, RS Turbo, Premier), com opções de motores 1.0 aspirado ou turbo, e diferentes pacotes de equipamentos. 👍`
     }
 }
 
-module.exports = Onix;
+module.exports = Onix
