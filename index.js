9de7c38f46be22437d041cff7412deb301eba190
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2025-09-24 20:01:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const prompt = require("prompt-sync")()
 
-
 const Onix = require("./class/Onix")
 const Polo = require("./class/Polo")
 const Strada = require("./class/Strada")
-
 
 const carros = [new Onix(), new Polo(), new Strada()]
 
@@ -17,7 +15,7 @@ const resposta = prompt("Digite o número: ")
 const indice = parseInt(resposta) - 1
 
 if (indice >= 0 && indice < carros.length) {
-  console.log(` Você escolheu: ${carros[indice].nome}`)
+  console.log(`Você escolheu: ${carros[indice].acessoPainel()}`)
 } else {
-  console.log(" Opção inválida!"
+  console.log("Opção inválida!")
 }
