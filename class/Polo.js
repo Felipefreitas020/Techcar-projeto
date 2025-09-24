758f8cecbd0a71f6b30f4e00df27cdddd939c3e9
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2025-09-24 20:03:45 -0300

    Update Polo.js

--- class/Polo.js
+++ class/Polo.js
@@ -1,14 +1,16 @@
-const carros = require('./Carros');
+const Carros = require('./Carros')
 
-class Polo extends carros {
-    constructor(descricao){
-    super(descricao)
+class Polo extends Carros {
+    constructor() {
+        super(
+            "Polo ","O Volkswagen Polo é um dos principais concorrentes do Chevrolet Onix no mercado de hatches compactos, oferecendo um equilíbrio entre dirigibilidade, tecnologia e economia." 
+        )
     }
 
-    acessoPainel(){
+    acessoPainel() {
         return `
-            O Volkswagen Polo é um dos principais concorrentes do Chevrolet Onix no mercado de hatches compactos, oferecendo um equilíbrio entre dirigibilidade, tecnologia 
-            e economia com versões (Track, MPI,TSI, Comfortline, Highline, GTS ) 👍`
+${this.nome}: ${this.descricao}
+Versões disponíveis: Track, MPI, TSI, Comfortline, Highline, GTS. 👍`
     }
 }
 
